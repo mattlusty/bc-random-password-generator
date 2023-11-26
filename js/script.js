0dd5f5db73a36f33c95b23eaad9007eda51bec4a
mattlusty <mattlusty123@gmail.com>
2023-11-26 20:10:52 +0000

    Removed Obselete Starter code

--- js/script.js
+++ js/script.js
@@ -107,33 +107,3 @@ function getRandomNumber(min, max) {
 function removeItem(array, index) {
   return array.splice(index, 1);
 }
-
-// Original Starter Code
-// =========================================================================
-
-// Function to prompt user for password options
-function getPasswordOptions() {}
-
-// IMPLEMENTED
-// // Function for getting a random element from an array
-// function getRandom(arr) {}
-
-// IMPLEMENTED
-// // Function to generate password with user input
-// function generatePassword() {}
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// IMPLEMENTED
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-// }
-
-// ADDED
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
